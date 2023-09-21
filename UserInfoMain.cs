ad91b4bfc58c12711883e8bca85dfaea227d3b46
Yasuyuki <ontninjao@gmail.com>
2023-09-21 11:16:06 +0700

    rt 21/09

--- UserInfoMain.cs
+++ UserInfoMain.cs
@@ -46,6 +46,15 @@ namespace UserInfo
                 this.axCZKEM1.OnFinger -= new zkemkeeper._IZKEMEvents_OnFingerEventHandler(axCZKEM1_OnFinger);
                 this.axCZKEM1.OnVerify -= new zkemkeeper._IZKEMEvents_OnVerifyEventHandler(axCZKEM1_OnVerify);
                 this.axCZKEM1.OnAttTransactionEx += new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
+                this.axCZKEM1.OnFingerFeature += new zkemkeeper._IZKEMEvents_OnFingerFeatureEventHandler(axCZKEM1_OnFingerFeature);
+                this.axCZKEM1.OnEnrollFingerEx += new zkemkeeper._IZKEMEvents_OnEnrollFingerExEventHandler(axCZKEM1_OnEnrollFingerEx);
+                this.axCZKEM1.OnDeleteTemplate += new zkemkeeper._IZKEMEvents_OnDeleteTemplateEventHandler(axCZKEM1_OnDeleteTemplate);
+                this.axCZKEM1.OnNewUser += new zkemkeeper._IZKEMEvents_OnNewUserEventHandler(axCZKEM1_OnNewUser);
+                this.axCZKEM1.OnHIDNum += new zkemkeeper._IZKEMEvents_OnHIDNumEventHandler(axCZKEM1_OnHIDNum);
+                this.axCZKEM1.OnAlarm += new zkemkeeper._IZKEMEvents_OnAlarmEventHandler(axCZKEM1_OnAlarm);
+                this.axCZKEM1.OnDoor += new zkemkeeper._IZKEMEvents_OnDoorEventHandler(axCZKEM1_OnDoor);
+                this.axCZKEM1.OnWriteCard += new zkemkeeper._IZKEMEvents_OnWriteCardEventHandler(axCZKEM1_OnWriteCard);
+                this.axCZKEM1.OnEmptyCard += new zkemkeeper._IZKEMEvents_OnEmptyCardEventHandler(axCZKEM1_OnEmptyCard);
 
                 bIsConnected = false;
                 btnConnect.Text = "Connect";
@@ -69,6 +78,15 @@ namespace UserInfo
                     this.axCZKEM1.OnFinger += new zkemkeeper._IZKEMEvents_OnFingerEventHandler(axCZKEM1_OnFinger);
                     this.axCZKEM1.OnVerify += new zkemkeeper._IZKEMEvents_OnVerifyEventHandler(axCZKEM1_OnVerify);
                     this.axCZKEM1.OnAttTransactionEx += new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
+                    this.axCZKEM1.OnFingerFeature += new zkemkeeper._IZKEMEvents_OnFingerFeatureEventHandler(axCZKEM1_OnFingerFeature);
+                    this.axCZKEM1.OnEnrollFingerEx += new zkemkeeper._IZKEMEvents_OnEnrollFingerExEventHandler(axCZKEM1_OnEnrollFingerEx);
+                    this.axCZKEM1.OnDeleteTemplate += new zkemkeeper._IZKEMEvents_OnDeleteTemplateEventHandler(axCZKEM1_OnDeleteTemplate);
+                    this.axCZKEM1.OnNewUser += new zkemkeeper._IZKEMEvents_OnNewUserEventHandler(axCZKEM1_OnNewUser);
+                    this.axCZKEM1.OnHIDNum += new zkemkeeper._IZKEMEvents_OnHIDNumEventHandler(axCZKEM1_OnHIDNum);
+                    this.axCZKEM1.OnAlarm += new zkemkeeper._IZKEMEvents_OnAlarmEventHandler(axCZKEM1_OnAlarm);
+                    this.axCZKEM1.OnDoor += new zkemkeeper._IZKEMEvents_OnDoorEventHandler(axCZKEM1_OnDoor);
+                    this.axCZKEM1.OnWriteCard += new zkemkeeper._IZKEMEvents_OnWriteCardEventHandler(axCZKEM1_OnWriteCard);
+                    this.axCZKEM1.OnEmptyCard += new zkemkeeper._IZKEMEvents_OnEmptyCardEventHandler(axCZKEM1_OnEmptyCard);
                 }
             }
             else
@@ -680,6 +698,113 @@ namespace UserInfo
             lbRTShow.Items.Add("...VerifyMethod:" + iVerifyMethod.ToString());
             lbRTShow.Items.Add("...Workcode:" + iWorkCode.ToString());//the difference between the event OnAttTransaction and OnAttTransactionEx
             lbRTShow.Items.Add("...Time:" + iYear.ToString() + "-" + iMonth.ToString() + "-" + iDay.ToString() + " " + iHour.ToString() + ":" + iMinute.ToString() + ":" + iSecond.ToString());
+        }
+
+        //When you have enrolled your finger,this event will be triggered and return the quality of the fingerprint you have enrolled
+        private void axCZKEM1_OnFingerFeature(int iScore)
+        {
+            if (iScore < 0)
+            {
+                lbRTShow.Items.Add("The quality of your fingerprint is poor");
+            }
+            else
+            {
+                lbRTShow.Items.Add("RTEvent OnFingerFeature Has been Triggered...Score:　" + iScore.ToString());
+            }
+        }
+
+        //When you are enrolling your finger,this event will be triggered.(The event can only be triggered by TFT screen devices)
+        private void axCZKEM1_OnEnrollFingerEx(string sEnrollNumber, int iFingerIndex, int iActionResult, int iTemplateLength)
+        {
+            if (iActionResult == 0)
+            {
+                lbRTShow.Items.Add("RTEvent OnEnrollFigerEx Has been Triggered....");
+                lbRTShow.Items.Add(".....UserID: " + sEnrollNumber + " Index: " + iFingerIndex.ToString() + " tmpLen: " + iTemplateLength.ToString());
+            }
+            else
+            {
+                lbRTShow.Items.Add("RTEvent OnEnrollFigerEx Has been Triggered Error,actionResult=" + iActionResult.ToString());
+            }
+        }
+
+        //When you have deleted one one fingerprint template,this event will be triggered.
+        private void axCZKEM1_OnDeleteTemplate(int iEnrollNumber, int iFingerIndex)
+        {
+            lbRTShow.Items.Add("RTEvent OnDeleteTemplate Has been Triggered...");
+            lbRTShow.Items.Add("...UserID=" + iEnrollNumber.ToString() + " FingerIndex=" + iFingerIndex.ToString());
+        }
+
+        //When you have enrolled a new user,this event will be triggered.
+        private void axCZKEM1_OnNewUser(int iEnrollNumber)
+        {
+            lbRTShow.Items.Add("RTEvent OnNewUser Has been Triggered...");
+            lbRTShow.Items.Add("...NewUserID=" + iEnrollNumber.ToString());
+        }
+
+        //When you swipe a card to the device, this event will be triggered to show you the card number.
+        private void axCZKEM1_OnHIDNum(int iCardNumber)
+        {
+            lbRTShow.Items.Add("RTEvent OnHIDNum Has been Triggered...");
+            lbRTShow.Items.Add("...Cardnumber=" + iCardNumber.ToString());
+        }
+
+        //When the dismantling machine or duress alarm occurs, trigger this event.
+        private void axCZKEM1_OnAlarm(int iAlarmType, int iEnrollNumber, int iVerified)
+        {
+            lbRTShow.Items.Add("RTEvnet OnAlarm Has been Triggered...");
+            lbRTShow.Items.Add("...AlarmType=" + iAlarmType.ToString());
+            lbRTShow.Items.Add("...EnrollNumber=" + iEnrollNumber.ToString());
+            lbRTShow.Items.Add("...Verified=" + iVerified.ToString());
+        }
+
+        //Door sensor event
+        private void axCZKEM1_OnDoor(int iEventType)
+        {
+            lbRTShow.Items.Add("RTEvent Ondoor Has been Triggered...");
+            lbRTShow.Items.Add("...EventType=" + iEventType.ToString());
+        }
+
+        //When you have emptyed the Mifare card,this event will be triggered.
+        private void axCZKEM1_OnEmptyCard(int iActionResult)
+        {
+            lbRTShow.Items.Add("RTEvent OnEmptyCard Has been Triggered...");
+            if (iActionResult == 0)
+            {
+                lbRTShow.Items.Add("...Empty Mifare Card OK");
+            }
+            else
+            {
+                lbRTShow.Items.Add("...Empty Failed");
+            }
+        }
+
+        //When you have written into the Mifare card ,this event will be triggered.
+        private void axCZKEM1_OnWriteCard(int iEnrollNumber, int iActionResult, int iLength)
+        {
+            lbRTShow.Items.Add("RTEvent OnWriteCard Has been Triggered...");
+            if (iActionResult == 0)
+            {
+                lbRTShow.Items.Add("...Write Mifare Card OK");
+                lbRTShow.Items.Add("...EnrollNumber=" + iEnrollNumber.ToString());
+                lbRTShow.Items.Add("...TmpLength=" + iLength.ToString());
+            }
+            else
+            {
+                lbRTShow.Items.Add("...Write Failed");
+            }
+        }
+
+        //After function GetRTLog() is called ,RealTime Events will be triggered. 
+        //When you are using these two functions, it will request data from the device forwardly.
+        private void rtTimer_Tick(object sender, EventArgs e)
+        {
+            if (axCZKEM1.ReadRTLog(iMachineNumber))
+            {
+                while (axCZKEM1.GetRTLog(iMachineNumber))
+                {
+                    ;
+                }
+            }
         }
 
         #endregion
